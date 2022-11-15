9f81c9df70dbf8fc7f5da901fd54e928ccfc5f76
zksu <565338107@qq.com>
2022-11-15 10:59:40 +0800

    feat: 通过 babel 解决 路径等问题

---
+++ .babelrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    '@babel/plugin-proposal-export-namespace-from',
+    // 'react-native-reanimated/plugin',
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '~': './src/',
+          '~locales': './src/locales/',
+        },
+      },
+    ],
+  ],
+  env: {
+    production: {
+      // 移除所有打印
+      plugins: ['transform-remove-console'],
+    },
+  },
+};
